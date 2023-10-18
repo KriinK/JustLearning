2093ad04d38def998c11573b9d5a90201c53c212
kriinkayker@gmail.com <kriinkayker@gmail.com>
2023-10-17 22:32:42 -0300

    First JavaScript commands

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+//We can change some html tag using:
+document.getElementById("Jss").innerHTML = "We can change some HTML tag through JavaScript!"
+
+document.getElementById("changeText").innerHTML = "Text changed by JS"
+
+
